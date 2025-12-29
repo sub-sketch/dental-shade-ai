c0d8c2f946ec37ff7938ffeaaa2c5f4ec6de47f6
sub-sketch <sub@subdesigncenter.com>
2025-12-29 21:56:24 +0900

    Add files via upload

---
+++ run_app.py
@@ -0,0 +1,16 @@
+import streamlit.web.cli as stcli
+import os, sys
+
+def resolve_path(path):
+    resolved_path = os.path.abspath(os.path.join(os.getcwd(), path))
+    return resolved_path
+
+if __name__ == "__main__":
+    # app.py를 실행하는 명령어 설정
+    sys.argv = [
+        "streamlit",
+        "run",
+        resolve_path("app.py"),
+        "--global.developmentMode=false",
+    ]
+    sys.exit(stcli.main())
